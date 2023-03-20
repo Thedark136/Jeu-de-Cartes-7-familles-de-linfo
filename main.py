5b3d0578d37d859f1a087485b8852707e2282a6e
zouhair-bit <64888405+zouhair-bit@users.noreply.github.com>
2023-03-20 18:20:51 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ root.title('Nsi Project 3')
 root.geometry("1000x550")
 root.configure(background="#F5F1ED")
 
+def init():
+    global joueur1_image
+    joueur1_image = resize_cards(f'Cartes/back.png')
+    joueur1_label.config(image=joueur1_image)
+
+    global joueur2_image
+    joueur2_image = resize_cards(f'Cartes/back.png')
+    joueur2_label.config(image=joueur2_image)
+
 # Resize Cards
 def resize_cards(card):
 	# Open the image
@@ -60,43 +69,43 @@ def commencer():
     for i in range (0,len(cartes),2):
         joueur_1.append(cartes[i])
         joueur_2.append(cartes[i+1])
-    global player1_image
-    player1_image = resize_cards(f'Cartes/back.png')
-    player1_label.config(image=player1_image)
+        
+    global joueur1_image
+    joueur1_image = resize_cards(f'Cartes/back.png')
+    joueur1_label.config(image=joueur1_image)
     
-    global player2_image
-    player2_image = resize_cards(f'Cartes/back.png')
-    player2_label.config(image=player2_image)
+    global joueur2_image
+    joueur2_image = resize_cards(f'Cartes/back.png')
+    joueur2_label.config(image=joueur2_image)
     
     start_button.config(text="Rejouer")
-     
+
 
 def Jouer():
 
     joueur_1_victoire = 0
     joueur_2_victoire = 0
     
-    player1_frame.config(text=f"Joueur 1 - Victoires : {joueur_1_victoire}")
-    player2_frame.config(text=f"Joueur 2 - Victoires : {joueur_2_victoire}")
+    root.title(f'Projet NSI 3 - Victoires du Joueur 1 : {joueur_1_victoire}, Victoires du Joueur 2: {joueur_1_victoire}')
 	
     if len(joueur_1)==0:
         joueur_2_victoire+=1
-        player1_frame.config(text=f"Joueur 1 - Victoires : {joueur_1_victoire}")
-        player2_frame.config(text=f"Joueur 2 - Victoires : {joueur_2_victoire}")
+        root.title(f'Projet NSI 3 - Victoires du Joueur 1 : {joueur_1_victoire}, Victoires du Joueur 2: {joueur_1_victoire}')
         return ("Joueur 2 a gagné le match")
+    
     elif len(joueur_2)==0:
         joueur_1_victoire+=1
-        player1_frame.config(text=f"Joueur 1 - Victoires : {joueur_1_victoire}")
-        player2_frame.config(text=f"Joueur 2 - Victoires : {joueur_2_victoire}")
+        root.title(f'Projet NSI 3 - Victoires du Joueur 1 : {joueur_1_victoire}, Victoires du Joueur 2: {joueur_1_victoire}')
         return("Joueur 1 a gagné le match")
     
-    global player1_image
-    player1_image = resize_cards(f'Cartes/{joueur_1[0][0]}.png')
-    player1_label.config(image=player1_image)
     
-    global player2_image
-    player2_image = resize_cards(f'Cartes/{joueur_2[0][0]}.png')
-    player2_label.config(image=player2_image)
+    global joueur1_image
+    joueur1_image = resize_cards(f'Cartes/{joueur_1[0][0]}.png')
+    joueur1_label.config(image=joueur1_image)
+    
+    global joueur2_image
+    joueur2_image = resize_cards(f'Cartes/{joueur_2[0][0]}.png')
+    joueur2_label.config(image=joueur2_image)
 
     if joueur_1[0][1]>joueur_2[0][1]:
         print("Joueur 1 gagne la manche")
@@ -113,43 +122,49 @@ def Jouer():
         joueur_1.append(joueur_1.pop(0))
         joueur_2.append(joueur_2.pop(0))
     
-    root.title(f'Projet NSI 3 - {len(joueur_1)} restantes au joueur 1, {len(joueur_2)} restantes au joueur 2')
+    joueur1_frame.config(text=f"Joueur 1 - Cartes :  {len(joueur_1)}")
+    joueur2_frame.config(text=f"Joueur 2 - Cartes :  {len(joueur_2)}")
     
+    
+#dangerzone 
 def Jouer_loop():
-    pass
-
+    while joueur_1 != 0 and joueur_2 != 0:
+        Jouer()
+        time.sleep(0.001)
+#dangerzone
 
 my_frame = Frame(root, bg="#F5F1ED")
 my_frame.pack(pady=20)
 
 # Create Frames For Cards
-player1_frame = LabelFrame(my_frame, text="Player 1" , fg='white', bd=0, bg="#252323")
-player1_frame.grid(row=0, column=0, padx=20, ipadx=30)
+joueur1_frame = LabelFrame(my_frame, text="Player 1" , fg='white', bd=0, bg="#252323")
+joueur1_frame.grid(row=0, column=0, padx=20, ipadx=30)
 
-player2_frame = LabelFrame(my_frame, text="Player 2" , fg='white', bd=0, bg="#252323")
-player2_frame.grid(row=0, column=1, ipadx=30)
+joueur2_frame = LabelFrame(my_frame, text="Player 2" , fg='white', bd=0, bg="#252323")
+joueur2_frame.grid(row=0, column=1, ipadx=30)
 
 # Put cards in frames
-player1_label = Label(player1_frame, text='')
-player1_label.pack(pady=20)
+joueur1_label = Label(joueur1_frame, text='')
+joueur1_label.pack(pady=20)
 
-player2_label = Label(player2_frame, text='')
-player2_label.pack(pady=20)
+joueur2_label = Label(joueur2_frame, text='')
+joueur2_label.pack(pady=20)
 
 
 # Create a couple buttons
 jouer_button = Button(root, text="Jouer", font=("Helvetica", 14), command=Jouer)
-jouer_button.pack(pady=13)
+jouer_button.pack(pady=10)
 
 loop_button = Button(root, text="Jouer automatiquement", font=("Helvetica", 14), command=Jouer_loop)
-loop_button.pack(pady=13)
+loop_button.pack(pady=10)
 
 start_button = Button(root, text="Commencer", font=("Helvetica", 14), command=commencer)
-start_button.pack(pady=13)
+start_button.pack(pady=10)
 
 
 
 
 
 
+init()
 root.mainloop()
