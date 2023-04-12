0e9295a0b0c31479e2ce43c04e0f03a979216a81
Wissam-D <wissaduk@gmail.com>
2023-04-12 20:57:37 +0300

    Game Mechanics Update
    Changed the dictionary
    made the quit button smaller
    added a function to determine when the game ends + function to draw 2 cards from graveyard
    to do list at bottom

--- secondGame.py
+++ secondGame.py
@@ -114,7 +114,7 @@ holderLabel1.grid(row= 1, column= 0)
 holderLabel2.grid(row= 1, column= 2)
 startButton = Button(mainGrid, text="Commencer", command=deckDisplay, width=30, font=("Helvetica", 18), borderwidth=0)
 startButton.grid(row= 1, column= 1)
-quitButton = Button(mainGrid, text="Sortir", command=Quit, width=30, font=("Helvetica", 18), borderwidth=0)
+quitButton = Button(mainGrid, text="Sortir", command=Quit, width=20, font=("Helvetica", 18), borderwidth=0)
 quitButton.grid(row= 2, column= 1)
 listCards = [
     ("Jules César",1),("AL-Kindi",2),("Diffie Hellman",3),("Rivest-Shamir-Adleman (RSA)",4),
@@ -128,17 +128,110 @@ listCards = [
     ("Alexander Graham Bell",1),("Claude Shannon",2), ("Vinton Cerf",3), ("Tim Berners-Lee",4), 
     ("Pascale Vicat-Blanc",5), ("Anne-Marie Kermarrec",6)]
 dictionnaryCards = {
-    "Sécurité et confidentialité":[("Jules César",1),("AL-Kindi",2),("Diffie Hellman",3),("Rivest-Shamir-Adleman (RSA)",4),("Shafi Goldwasser",5),("Cynthia Dwork",6)],
-    "Algorithmes & Programmation":[("Al-Khwarizmi",1), ("Ada Lovelace",2), ("Grace Hopper",3), ("Dorothy Vaughan",4), ("Gilles Kahn",5), ("Gérard Berry",6)],
-    "Mathématiques & Informatique":[("Hypatie d'Alexandrie",1), ("George Boole",2), ("Alonzo Church",3), ("Jacques-Louis Lions",4), ("Ingrid Daubechies",5), ("Jocelyne Troccaz",6)],
-    "Intelligence Artificielle":[("Herbert Simon",1), ("Marvin Minsky",2), ("Geoffrey Hinton",3), ("Rose Dieng-Kuntz",4), ("Yann LeCun",5), ("Cordelia Schmid",6)],
-    "Machines & Composants":[("Charles Babbage",1), ("John von Neumann",2), ("Hedy Lamarr",3), ("Seymour Cray",4), ("Gordon Moore",5), ("Hiroshi Ishiguro",6)],
-    "Interaction Homme-Machine":[("Doug Engelbart",1), ("Ted Nelson",2), ("Alan Kay",3), ("Joëlle Coutaz",4), ("Jean-Marie Hullot",5), ("Marie-Paule Cani",6)],
-    "Systèmes & réseaux":[("Alexander Graham Bell",1), ("Claude Shannon",2), ("Vinton Cerf",3), ("Tim Berners-Lee",4), ("Pascale Vicat-Blanc",5), ("Anne-Marie Kermarrec",6)],
+    "Jules César" : ["Sécurité et confidentialité", 1],
+    "Al-Kindi" : ["Sécurité et confidentialité", 2],
+    "Diffie Hellman" : ["Sécurité et confidentialité", 3],
+    "Rivest-Shamir-Adleman (RSA)" : ["Sécurité et confidentialité", 4],
+    "Shafi Goldwasser" : ["Sécurité et confidentialité", 5],
+    "Cynthia Dwork" : ["Sécurité et confidentialité", 6],
+    "Al-Khwarizmi" : ["Algorithmes & Programmation", 1],
+    "Ada Lovelace" : ["Algorithmes & Programmation", 2],
+    "Grace Hopper" : ["Algorithmes & Programmation", 3],
+    "Dorothy Vaughan" : ["Algorithmes & Programmation", 4],
+    "Gilles Kahn" : ["Algorithmes & Programmation", 5],
+    "Gérard Berry": ["Algorithmes & Programmation", 6],
+    "Hypatie d'Alexandrie" : ["Mathématiques & Informatique", 1],
+    "George Boole" : ["Mathématiques & Informatique", 2],
+    "Alonzo Church" : ["Mathématiques & Informatique", 3],
+    "Jacques-Louis Lions" : ["Mathématiques & Informatique", 4],
+    "Ingrid Daubechies" : ["Mathématiques & Informatique", 5],
+    "Jocelyne Troccaz" : ["Mathématiques & Informatique", 6],
+    "Herbert Simon" : ["Intelligence Artificielle", 1],
+    "Marvin Minsky" : ["Intelligence Artificielle", 2],
+    "Geoffrey Hinton" : ["Intelligence Artificielle", 3],
+    "Rose Dieng-Kuntz" : ["Intelligence Artificielle", 4],
+    "Yann LeCun" : ["Intelligence Artificielle", 5],
+    "Cordelia Schmid" : ["Intelligence Artificielle", 6],
+    "Charles Babbage" : ["Machines & Composants", 1],
+    "John von Neumann" : ["Machines & Composants", 2],
+    "Hedy Lamarr" : ["Machines & Composants", 3],
+    "Seymour Cray" : ["Machines & Composants", 4],
+    "Gordon Moore" : ["Machines & Composants", 5],
+    "Hiroshi Ishiguro" : ["Machines & Composants", 6],
+    "Doug Engelbart" : ["Interaction Homme-Machine", 1],
+    "Ted Nelson" : ["Interaction Homme-Machine", 2],
+    "Alan Kay" : ["Interaction Homme-Machine", 3],
+    "Joëlle Coutaz" : ["Interaction Homme-Machine", 4],
+    "Jean-Marie Hullot" : ["Interaction Homme-Machine", 5],
+    "Marie-Paule Cani" : ["Interaction Homme-Machine", 6],
+    "Alexander Graham Bell" : ["Systèmes & réseaux", 1],
+    "Claude Shannon" : ["Systèmes & réseaux", 2],
+    "Vinton Cerf" : ["Systèmes & réseaux", 3],
+    "Tim Berners-Lee" : ["Systèmes & réseaux", 4],
+    "Pascale Vicat-Blanc" : ["Systèmes & réseaux", 5],
+    "Anne-Marie Kermarrec" : ["Systèmes & réseaux", 6],
 }
-# game mechanics - coming soon
-
+# game mechanics - not done
+# function that checks if anyone won the game
+def conditionCheck():
+    familyCount1 = {
+        "Sécurité et confidentialité":0,
+        "Algorithmes & Programmation":0,
+        "Mathématiques & Informatique":0,
+        "Intelligence Artificielle":0,
+        "Machines & Composants":0,
+        "Interaction Homme-Machine":0,
+        "Systèmes & réseaux":0
+    }
+    powerCount1 = {
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0,
+    }
+    for i in firstDeck :
+        familyCount1[dictionnaryCards[i[0]]] += 1
+        powerCount1[dictionnaryCards[i[1]]] += 1
+    # same but for the 2nd player
+    familyCount2 = {
+        "Sécurité et confidentialité":0,
+        "Algorithmes & Programmation":0,
+        "Mathématiques & Informatique":0,
+        "Intelligence Artificielle":0,
+        "Machines & Composants":0,
+        "Interaction Homme-Machine":0,
+        "Systèmes & réseaux":0
+    }
+    powerCount2 = {
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0,
+    }
+    for i in secondDeck :
+        familyCount2[dictionnaryCards[i[0]]] += 1
+        powerCount2[dictionnaryCards[i[1]]] += 1
     
-
+    if (familyCount1["Algorithmes & Programmation"] == 6 or familyCount1["Intelligence Artificielle"]==6 or familyCount1["Interaction Homme-Machine"] == 6 or familyCount1["Machines & Composants"]==6 or familyCount1["Mathématiques & Informatique"]==6 or familyCount1["Systèmes & réseaux"]==6 or familyCount1["Sécurité et confidentialité"]==6) and (familyCount2["Algorithmes & Programmation"] == 6 or familyCount2["Intelligence Artificielle"]==6 or familyCount2["Interaction Homme-Machine"] == 6 or familyCount2["Machines & Composants"]==6 or familyCount2["Mathématiques & Informatique"]==6 or familyCount2["Systèmes & réseaux"]==6 or familyCount2["Sécurité et confidentialité"]==6):
+        return('Egalité')
+    elif familyCount2["Algorithmes & Programmation"] == 6 or familyCount2["Intelligence Artificielle"]==6 or familyCount2["Interaction Homme-Machine"] == 6 or familyCount2["Machines & Composants"]==6 or familyCount2["Mathématiques & Informatique"]==6 or familyCount2["Systèmes & réseaux"]==6 or familyCount2["Sécurité et confidentialité"]==6:
+        return('Le joueur 2 a gagné !')
+    elif familyCount1["Algorithmes & Programmation"] == 6 or familyCount1["Intelligence Artificielle"]==6 or familyCount1["Interaction Homme-Machine"] == 6 or familyCount1["Machines & Composants"]==6 or familyCount1["Mathématiques & Informatique"]==6 or familyCount1["Systèmes & réseaux"]==6 or familyCount1["Sécurité et confidentialité"]==6:
+        return('Le joueur 1 a gagné')
+# draw 2 cards from cemetary
+def drawCards():
+    return ([cemetary[0], cemetary[1]])
+# to do -
+# function to compare the cards (+ button), inside ya 5 fonctions :
+# 1 - fonction qui montre les choix du gagnats sous formes de 2 bouttons
+# 2 -  2 fonctions pour les deux choix (check grp whatsapp)
+#  une fonction en plus pour le choix du joueur sur kel carte il guve away dans les deux case   
+# 1 fonction pour le cas degalite (incorporated with 'drawCards()')
+# fonction qui met ensemble conditionCheck et comparaison ensemble pour representer le jeu en totalite 
+# ^(probably ajouter un boutton pour ca kamen)/(might change le return de conditionCheck to numbers bhal 7ale)
 init()
 root.mainloop()
